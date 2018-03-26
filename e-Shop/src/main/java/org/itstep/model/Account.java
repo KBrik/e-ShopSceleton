5bda1543873830bb14d0a3afbce39f9003dd5d91
AnastasiaKozachuk <32028221+AnastasiaKozachuk@users.noreply.github.com>
2018-03-26 20:59:20 +0300

    Change Account

--- e-Shop/src/main/java/org/itstep/model/Account.java
+++ e-Shop/src/main/java/org/itstep/model/Account.java
@@ -1,5 +1,26 @@
 package org.itstep.model;
 
-public class Account {
+import com.fasterxml.jackson.annotation.JsonProperty;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class Account {
+	
+	@JsonProperty
+	private String login;
+	@JsonProperty
+	private String password;
+	@JsonProperty
+	private String firstName;
+	@JsonProperty
+	private String secondName;
+	@JsonProperty
+	private String telephone;
+	
+	public Account() {
+	}
+	
 }
